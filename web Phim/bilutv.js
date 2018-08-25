6a1587fc3da810a204cc6d980efb83e016c092cd
phong-php <nvphongbk.com>
2018-08-25 15:52:19 +0700

    hello

---
+++ web Phim/bilutv.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $(".theLoai ").mouseenter(function(){
+     $("#theLoai").show();
+    });
+});
+$(document).ready(function(){
+    $(".theLoai ").mouseleave(function(){
+     $("#theLoai").hide();
+    });
+});
+$(document).ready(function(){
+    $(".quocGia ").mouseenter(function(){
+     $("#quocGia").show();
+    });
+});
+$(document).ready(function(){
+    $(".quocGia ").mouseleave(function(){
+     $("#quocGia").hide();
+    });
+});
+$(document).ready(function(){
+    $(".phimLe ").mouseenter(function(){
+     $("#phimLe").show();
+    });
+});
+$(document).ready(function(){
+    $(".phimLe ").mouseleave(function(){
+     $("#phimLe").hide();
+    });
+});
+$(document).ready(function(){
+    $(".phimBo ").mouseenter(function(){
+     $("#phimBo").show();
+    });
+});
+$(document).ready(function(){
+    $(".phimBo ").mouseleave(function(){
+     $("#phimBo").hide();
+    });
+});
